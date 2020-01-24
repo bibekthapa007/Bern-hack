a3344aa2fa17b82c8e3af86f8db5ef0aa38e4436
lighting999 <gauravpandey2058@gmail.com>
2020-01-24 13:30:03 +0545

    'initial'

---
+++ Frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import LoginForm from "./components/loginform";
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/login">
+        <LoginForm />
+      </Route>
+      <Route path="/dashboard/">
+        <Dashboard />
+      </Route>
+      <Route path="/link/:linkid">
+        <Dashboard />
+      </Route>
+    </Switch>
+  );
+}
+
+function Home() {
+  return <div></div>;
+}
+
+function Dashboard() {
+  return <div></div>;
+}
+
+export default App;
